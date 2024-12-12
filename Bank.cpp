46cf8f98b65acb8dfea4f4872a890412755d80ed
PayalPatil8980 <sq.payal30@gmail.com>
2024-12-12 08:14:14 +0530

    last updated

--- Bank.cpp
+++ Bank.cpp
@@ -9,25 +9,51 @@ class Bank{
     int bal2=30000;
     int bal3=40000;
     public:
-     int n1,n2,n3,m1,m3,totalbal,diposit1;
-    void with(int pin){
-       
+    int n1,n2,n3,m1,m3,totalbal,diposit1;
+    void with(int pin){ 
         if(pin==p001){
             cout<<"Enter the Amouunt for withdraw: ";
             cin>>n1;
-        totalbal=bal1-n1;
+            if(bal1>n1){
+                bal1=bal1-n1;
+                cout<<"------------------------"<<endl;
+                cout<<"Remain Balace: "<<bal1<<endl;
+                cout<<"------------------------"<<endl;
+            }
+            else{
+                cout<<"you don't have inuf balance"<<endl;
+            }
+        //totalbal=bal1-n1;
         }
         else if(pin==p002){
             cout<<"Enter the Amouunt for withdraw: ";
             cin>>n1;
-        totalbal=bal2-n1;
+            if(bal2>n1){
+                bal2=bal2-n1;
+                cout<<"------------------------"<<endl;
+                cout<<"Remain Balance: "<<bal2<<endl;
+                cout<<"------------------------"<<endl;
+            }
+            else{
+                cout<<"you don't have inuf balance"<<endl;
+            }
+        //totalbal=bal2-n1;
         }
         else if(pin==p003){
             cout<<"Enter the Amouunt for withdraw: ";
             cin>>n1;
-        totalbal=bal3-n1;
+            if(bal3>n1){
+                bal3=bal3-n1;
+                cout<<"------------------------"<<endl;
+                cout<<"Remain Balance: "<<bal3<<endl;
+                cout<<"------------------------"<<endl;
+            }
+            else{
+                cout<<"you don't have inuf balance";
+            }
+        //totalbal=bal3-n1;
         }
-        cout<<"Total is"<<totalbal<<endl;
+        
 
     }
     void diposit(int pin){
@@ -35,60 +61,45 @@ class Bank{
         if(pin==p001){
             cout<<"Enter the Amouunt for diposit: ";
             cin>>n2;
-        diposit1=bal1+n2;
+            bal1=bal1+n2;
+            cout<<"------------------------"<<endl;
+            cout<<"diposit Money is: "<<n2<<endl;
+            cout<<"------------------------"<<endl;
         }
         else if(pin==p002){
             cout<<"Enter the Amouunt for diposit: ";
             cin>>n2;
-        diposit1=bal1+n2;
+            bal2=bal2+n2;
+            cout<<"------------------------"<<endl;
+            cout<<"diposit Money is: "<<n2<<endl;
+            cout<<"------------------------"<<endl;
         }
         else if(pin==p003){
             cout<<"Enter the Amouunt for diposit: ";
             cin>>n2;
-        diposit1=bal1+n2;
+            bal3=bal3+n2;
+            cout<<"------------------------"<<endl;
+            cout<<"diposit Money is: "<<n2<<endl;
+            cout<<"------------------------"<<endl;
         }
-        cout<<"Total deposit is: "<<diposit1<<endl;
+        
     }
     void balance(int pin){
         
         if(pin==p001){
-            if(bal1>totalbal){
-                m1=bal1-totalbal;
-                cout<<"Current balace is: "<<m1<<endl;
-            }
-            else if(bal1<diposit1){
-                m3=bal1+diposit1;
-                cout<<"current balance is: "<<m3<<endl;
-            }
-            else{
-                cout<<"current balance is: "<<bal1;
-            }
+            //cout<<"------------------------"<<endl;
+            cout<<"current balance is :"<<bal1<<endl;
+            cout<<"------------------------"<<endl;
         }
         else if(pin==p002){
-            if(bal1>totalbal){
-                m1=bal1-totalbal;
-                cout<<"Current balace is: "<<m1<<endl;
-            }
-            else if(bal1<diposit1){
-                m3=bal1+diposit1;
-                cout<<"current balance is: "<<m3<<endl;
-            }
-            else{
-                cout<<"current balance is: "<<bal1<<endl;
-            }
+            //cout<<"------------------------"<<endl;
+            cout<<"current balance is :"<<bal2<<endl;
+            cout<<"------------------------"<<endl;
         }
         else if(pin==p003){
-            if(bal1>totalbal){
-                m1=bal1-totalbal;
-                cout<<"Current balace is: "<<m1<<endl;
-            }
-            else if(bal1<diposit1){
-                m3=bal1+diposit1;
-                cout<<"current balance is: "<<m3<<endl;
-            }
-            else{
-                cout<<"current balance is: "<<bal1<<endl;
-            }
+            //cout<<"------------------------"<<endl;
+            cout<<"current balance is :"<<bal3<<endl;
+            cout<<"------------------------"<<endl;
         }
     }
 };
@@ -106,11 +117,12 @@ int main(){
     if(pin==003){
             cout<<"Krishna-40000"<<endl;
         }
-        
+    cout<<"------------------"<<endl; 
     cout<<"1 for withdraw"<<endl;
     cout<<"2 for Diposit"<<endl;
     cout<<"3 for Blance Check"<<endl;
     cout<<"4 for Exit"<<endl;
+    cout<<"------------------"<<endl;
     if(pin==001||pin==002||pin==003){
     while(n!=0){
         cout<<"Enter your choice:";
