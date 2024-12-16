55298b333c41cf355ad08bbe15fcc6017325ade5
PayalPatil8980 <sq.payal30@gmail.com>
2024-12-16 10:47:04 +0530

    16/12

---
+++ login.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Login{
+    string username;
+    string password;
+    string confirmpassword;
+    public:
+    void signup(){
+        cout<<"Enter username: ";
+        cin>>username;
+        cout<<"Enter password: ";
+        cin>>password;
+        cout<<"confirm your password:";
+        cin>>confirmpassword;
+        if (confirmpassword==password){
+            cout<<"Your account created successfully!"<<endl;   
+        }
+        else{
+            cout<<"fill password correctly!"<<endl;
+        }
+
+    }
+    void login(){
+        string inputusername,inputpassword;
+        cout<<"Enter username: ";
+        cin>>inputusername;
+        cout<<"Enter Password: ";
+        cin>>inputpassword;
+        if(inputusername==username && inputpassword==password){
+            cout<<"you are logged in successfully!"<<endl;
+        }
+        else{
+            cout<<"incorrect username or password!"<<endl;
+        }
+    }
+    void forgetpassword(){
+        string inputusername;
+
+        cout<<"Enter the username: ";
+        cin>>inputusername;
+
+        if(inputusername==username){
+            cout<<"your password is: "<<password<<endl;
+        }
+        else{
+            cout<<"Invalid username first signup!"<<endl;
+        }
+    }
+    void changepass(){
+        string inputusername,newpassword;
+        cout<<"Enter username: ";
+        cin>>inputusername;
+
+        if(inputusername==username){
+            
+            cout<<"Enter your new password: ";
+            cin>>newpassword;
+            password=newpassword;
+            cout<<"your password changed: "<<password<<endl;
+        }
+        else{
+            cout<<"please enter the correct username"<<endl;
+        }
+    }
+};
+int main(){
+    int i,n;
+    Login l1;
+    cout<<"-------------------------"<<endl;
+    cout<<"1 for signup"<<endl;
+    cout<<"2 for login"<<endl;
+    cout<<"3 for forgetpassword"<<endl;
+    cout<<"4 for change password"<<endl;
+    cout<<"0 for Exit"<<endl;
+    cout<<"-------------------------"<<endl;
+    while(n!=0){
+        cout<<"Enter your choice: ";
+        cin>>n;
+
+        switch (n)
+        {
+        case 1:
+            l1.signup();
+            break;
+        case 2:
+            l1.login();
+            break;
+        case 3:
+            l1.forgetpassword();
+            break;
+        case 4:
+            l1.changepass();
+        default:
+            cout<<"please enter the right choice"<<endl;
+            break;
+        }
+    }
+}
