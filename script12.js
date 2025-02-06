db384d72c60012c70e6d35d1dab7c68b14b365a5
PayalPatil8980 <sq.payal30@gmail.com>
2025-02-06 11:22:10 +0530

    06/02/2025

---
+++ script12.js
@@ -0,0 +1,17 @@
+let a=document.getElementById("img");
+
+function myfuncon(){
+    a.style.animationDuration=3+"s";
+}
+function myfuncoff(){
+    a.style.animationDuration=0+"s";
+}
+function myfunc2(){
+    a.style.animationDuration=1+"s";
+}
+function myfunc3(){
+    a.style.animationDuration=0.5+"s";
+}
+function myfunc4(){
+    a.style.animationDuration=0.1+"s";
+}
